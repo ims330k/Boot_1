a5c5c999d4ced0704f5591856d65a59822c21966
ims330k <ims330k@gmail.com>
2020-01-06 09:58:59 +0900

    Update CustomSchedule.java

--- src/main/java/com/iu/b1/schedule/CustomSchedule.java
+++ src/main/java/com/iu/b1/schedule/CustomSchedule.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component;
 public class CustomSchedule {
 
 	//@Scheduled(fixedRate = 1000)
-	@Scheduled(fixedRateString = "1000")
+	//@Scheduled(fixedRateString = "1000")
 	public void fixRateSchedule()throws Exception{
 		System.out.println("fixRate  "+Thread.currentThread().getName());
 	}
 	
-	@Scheduled(fixedDelay = 3000)
+	//@Scheduled(fixedDelay = 3000)
 	public void fixedDelaySchedule()throws Exception{
 		System.out.println("fix Delay  "+Thread.currentThread().getName());
 	}
 	
-	@Scheduled(cron = "*/3 * * * * *")
+	//@Scheduled(cron = "*/3 * * * * *")
 	public void cronSchedule()throws Exception{
 		System.out.println("쉬는시간 지킵시다....  "+Thread.currentThread().getName());
 	}
